3e8a5d3b5e7df69686e55ea26b630674ab34ca56
bpal13577 <bikash.pal@falconautoonline.com>
2023-03-04 00:49:51 +0530

    Add project files.

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SingletonDesignPattern;
+Thread t1 = new Thread(() =>
+ {
+     var instance = UploadServices.Instance();
+     Console.WriteLine(instance);
+ });
+Thread t2 = new Thread(() =>
+{
+    var instance = UploadServices.Instance();
+    Console.WriteLine(instance);
+});
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
---
+++ SingletonDesignPattern/UploadServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    class UploadServices
+    {
+        private UploadServices(){ }
+         
+        private static UploadServices _instance;
+        private static readonly object _lock =new  object();
+        public static UploadServices Instance()
+        {
+            if (_instance == null) 
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new UploadServices();
+                    }
+                }
+            } 
+            return _instance;
+        }
+    }
+}
